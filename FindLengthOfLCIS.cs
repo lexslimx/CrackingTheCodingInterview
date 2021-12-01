8ffe59f3390e6abbf85b6932cbfdad265984e4dd
alex <alex.km@live.com>
2021-12-01 09:40:49 +0300

    Added FindLengthOfLCIS

---
+++ FindLengthOfLCIS.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrackingTheCodingInterview
+{
+    public class FindLengthOfLCIS
+    {
+        public int FindLength(int[] nums)
+        {
+            if (nums.Length == 0) return 0;
+            int maxLength = 1; //2,3
+
+            int prev = nums[0]; //1,3,5
+
+            var temp = 1; //1,2,3
+                          //1,3,5,4,7
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] > prev)
+                {
+                    temp++;
+                    prev = nums[i];
+                    maxLength = Math.Max(temp, maxLength);
+                }
+                else
+                {
+                    maxLength = Math.Max(temp, maxLength);
+                    temp = 1;
+                    prev = nums[i];
+                }
+            }
+
+            return maxLength;
+        }
+    }
+}
---
+++ GroupAnagrams.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrackingTheCodingInterview
+{
+    internal class GroupAnagrams
+    {
+        //public IList<IList<string>> GroupAnagrams(string[] strs)
+        //{
+
+
+        //}
+    }
+}
