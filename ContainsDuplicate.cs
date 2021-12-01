f054e5c8cd14e5965e4bc542766c5c02f893c2e3
alex <alex.km@live.com>
2021-12-01 21:42:35 +0300

    Added contains duplicate

---
+++ ContainsDuplicate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrackingTheCodingInterview
+{
+    internal class ContainsDuplicate
+    {
+        public bool Contains(int[] nums)
+        {
+            if (nums.Length < 2) return false;
+            HashSet<int> mySet = new HashSet<int>();
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if (mySet.Contains(nums[i]))
+                {
+                    return true;
+                }
+                else
+                {
+                    mySet.Add(nums[i]);
+                }
+            }
+            return false;
+        }
+    }
+}
