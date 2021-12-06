06fcd5121a015e60e3a1cfecc72f0f0aa7404cdf
alex <alex.km@live.com>
2021-12-06 23:56:13 +0300

    Added sorted array to bst

---
+++ ArrayToBst.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrackingTheCodingInterview
+{
+    internal class ArrayToBst
+    {
+        public TreeNode SortedArrayToBST(int[] nums)
+        {
+            return AddNode(nums, 0, nums.Length - 1);
+        }
+
+        private TreeNode AddNode(int[] nums, int left, int right)
+        {
+            TreeNode root = null;
+            int mid = left + (right - left) / 2;
+            if (left <= right)
+            {
+                root = new TreeNode(nums[mid]);
+                root.left = AddNode(nums, left, mid - 1);
+                root.right = AddNode(nums, mid + 1, right);
+            }
+            return root;
+        }
+
+    }
+}
