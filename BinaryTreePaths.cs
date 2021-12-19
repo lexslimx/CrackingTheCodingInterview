895face74a12c59be4b61847ddee77742cdc674a
alex <alex.km@live.com>
2021-12-19 13:40:08 +0300

    Added Binary Tree Paths

---
+++ BinaryTreePaths.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using CrackingTheCodingInterview;
+
+public class BinaryTreePaths
+{
+    public IList<string> BinaryTreePaths(TreeNode root)
+    {
+        IList<string> list = new List<string>();
+        Build(root, "", list);
+
+        return list;
+    }
+    void Build(TreeNode root, string current,IList<string> list){
+        if(root == null){
+            return;
+        }
+        
+        if(root.left == null && root.right == null){
+            current += root.val;
+        }
+        else{
+             current+=root.val + "->";        
+        }
+        
+        if(root.left !=null) Build(root.left, current, list);
+    
+        if(root.right !=null) Build(root.right, current, list);
+        
+        if(root.left == null && root.right == null) list.Add(current);
+    }
+}
