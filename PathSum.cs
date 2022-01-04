71b2aa4dc68bd595309e2b3b04a1590480a11fc3
alex <alex.km@live.com>
2022-01-04 21:37:11 +0300

    Added path sum

---
+++ PathSum.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrackingTheCodingInterview
+{
+    internal class PathSum
+    {
+        public bool HasPathSum(TreeNode root, int targetSum)
+        {
+            if (root == null) { return false; }
+            if (root.left == null && root.right == null && targetSum == root.val) { return true; }
+            else if (root == null && targetSum != 0) { return false; }
+            else
+            {
+                return HasPathSum(root.left, targetSum - root.val) || HasPathSum(root.right, targetSum - root.val);
+            }
+        }
+    }
+}
